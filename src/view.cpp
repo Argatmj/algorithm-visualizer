cf929496d48df0ec4cc9f7d82f2dbf2a5d81ccc4
Argatmj <ikomangarga0612@gmail.com>
2024-11-14 14:38:40 +0100

    Added grids function and handle mouse event function

--- src/view.cpp
+++ src/view.cpp
@@ -1,13 +1,19 @@
 #include "view.h"
 
 view::view():
-height(400),
-width(400)
-{}
+window(sf::VideoMode(width, height), "SFML works!"),
+height(800),
+width(1000),
+cellSize(20),
+rows(height/20),
+cols(800/20)
+{
+    grid.resize(rows, std::vector<sf::RectangleShape>(cols));
+    initGrid();
+}
 
 
 void view::showWindow(){
-    sf::RenderWindow window(sf::VideoMode(height, width), "SFML works!");
     while (window.isOpen())
     {
         sf::Event event;
@@ -15,9 +21,52 @@ void view::showWindow(){
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if(event.type == sf::Event::MouseMoved || event.type == sf::Event::MouseButtonPressed){
+                handleMouseEvent();
+            }
         }
 
-        window.clear();
+        window.clear(sf::Color::White);
+        drawGrid();
         window.display();
+    }
+}
+
+void view::handleMouseEvent(){
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+    int gridX = mousePos.x / cellSize;
+    int gridY = mousePos.y / cellSize;
+
+    if (gridX >= 0 && gridX < cols && gridY >= 0 && gridY < rows) {
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            grid[gridY][gridX].setFillColor(sf::Color::Black);  
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+            grid[gridY][gridX].setFillColor(sf::Color::Green);  
+         }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::F)){
+            grid[gridY][gridX].setFillColor(sf::Color::Red);  
+        }
+    }
+}
+
+void view::drawGrid(){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0 ;j < cols; j++){
+                window.draw(grid[i][j]);
+            }
+        }
+}
+
+void view::initGrid(){
+    for(int y = 0; y < rows; y++){
+        for( int x = 0; x < cols ; x++){
+                grid[y][x].setSize(sf::Vector2f(cellSize, cellSize));
+                grid[y][x].setPosition(x * cellSize, y * cellSize);
+                grid[y][x].setFillColor(sf::Color::White); 
+                grid[y][x].setOutlineThickness(1);
+                grid[y][x].setOutlineColor(sf::Color::Black);
+        }
     }
 }
--- src/view.h
+++ src/view.h
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
 class view{
 public:
     view();
     void showWindow();
+    void initGrid();
+    void drawGrid();
+    void handleMouseEvent();
 
 private:
-    int height;
-    int width;
+    int height, width;
+    int cols, rows, cellSize;
+    sf::RenderWindow window;
+    std::vector<std::vector<sf::RectangleShape>> grid;
 };
